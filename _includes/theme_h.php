4f14d4b9a71d9b2f1523e72fe39a0d44cdf4aff2
G-Just <gjustas00@gmail.com>
2023-12-22 07:35:17 +0200

    Theme change redirection memory fix

--- _includes/theme_h.php
+++ _includes/theme_h.php
@@ -1,18 +1,25 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+print_r($location);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_COOKIE['theme'])) {
         if ($_COOKIE['theme'] == 'dark') {
-            setcookie("theme", "", time() - 3600);
-            setcookie('theme', 'light', 0, "/");
+            setcookie('theme', '', time() - 3600);
+            setcookie('theme', 'light', 0, '/');
         } else {
-            setcookie("theme", "", time() - 3600);
-            setcookie('theme', 'dark', 0, "/");
+            setcookie('theme', '', time() - 3600);
+            setcookie('theme', 'dark', 0, '/');
         }
     } else {
         setcookie('theme', 'dark', 0, "/");
     }
-    header('Location: ../index.php');
+    preg_match('/(?<=php-u2\/).*(?!\?)/', $_POST['from'], $location);
+    $location = (preg_split('/[?=]/', $location[0]));
+    if ($location[1] == 'wallet') {
+        header('Location: ../' . $location[0] . '?' . $location[1] . '=' . $location[2]);
+    } else {
+        header('Location: ../' . $location[0] ?? '');
+    }
 } else {
     header('Location: ../index.php?error=unauthorized');
     die();
